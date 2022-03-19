b393259eedaec94978bb12aec4b43fa33ec7148e
makiart <haruki3968@gmail.com>
2022-03-19 14:38:49 +0900

    add done chapter 03

--- common/layers.py
+++ common/layers.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import numpy as np
 from common.functions import softmax, cross_entropy_error
 
@@ -143,3 +144,22 @@ class Dropout:
 
     def backward(self, dout):
         return dout * self.mask
+
+
+class EmbeddingLayer:
+    def __init__(self, W) -> None:
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+    
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        dW[self.idx] = dout
+        return None
