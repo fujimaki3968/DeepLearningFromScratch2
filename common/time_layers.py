1c78b5880f3009f85d9ed1769d95c97ba6bde807
fujimaki3968 <s18160@tokyo.kosen-ac.jp>
2022-05-31 16:40:23 +0900

    add lstm layer

--- common/time_layers.py
+++ common/time_layers.py
@@ -1,7 +1,8 @@
+from tkinter.messagebox import NO
 import numpy as np
 
 from common.layers import EmbeddingLayer
-from common.functions import softmax
+from common.functions import sigmoid, softmax
 
 class RNN:
     def __init__(self, Wx, Wh, b) -> None:
@@ -200,3 +201,94 @@ class TimeSoftmaxWithLoss:
         dx = dx.reshape((N, T, V))
 
         return dx
+
+
+class LSTM:
+    def __init__(self, Wx, Wh, b) -> None:
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+    
+    def forward(self, x, h_prev, c_prev):
+        Wx, Wh, b = self.params
+        N, H = h_prev.shape
+        
+        A = np.dot(x, Wx) + np.dot(h_prev, Wh) + b
+        
+        # slice
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H:3*H]
+        o = A[:, 3*H:]
+        
+        f = sigmoid(f)
+        g = np.tanh(g)
+        i = sigmoid(i)
+        o = sigmoid(o)
+        
+        c_next = f * c_prev + g * i
+        h_next = o * np.tanh(c_next)
+        
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
+        return h_next, c_next
+
+
+class TimeLSTM:
+    def __init__(self, Wx, Wh, b, stateful=False) -> None:
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = None
+        self.h, self.c = None, None
+        self.dh = None
+        self.stateful = stateful
+    
+    def forward(self, xs):
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape
+        H = Wh.shape[0]
+        
+        self.layers = []
+        hs = np.empty((N, T, H), dtype="f")
+        
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype="f")
+        
+        if not self.stateful or self.c is None:
+            self.c = np.zeros((N, H), dtype="f")
+
+        for t in range(T):
+            layer = LSTM(*self.params)
+            self.h, self.c = layer.forward(xs[:, t, :], self.h, self.c)
+            hs[:, t, :] = self.h
+            
+            self.layers.append(layer)
+        
+        return hs
+    
+    def backward(self, dhs):
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D = Wx.shape[0]
+        
+        dxs = np.empty((N, T, D), dtype="f")
+        dh, dc = 0, 0
+        
+        grads = [0, 0, 0]
+        for t in reversed(T):
+            layer = self.layers[t]
+            dx, dh, dc = layer.backward(dhs[:, t, :] + dh, dc)
+            dxs[:, t, :] = dx
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
+        
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+        self.dh = dh
+        
+        return dxs
+    
+    def set_state(self, h, c=None):
+        self.h, self.c = h, c
+    
+    def reset_state(self):
+        self.h, self.c = None, None    
